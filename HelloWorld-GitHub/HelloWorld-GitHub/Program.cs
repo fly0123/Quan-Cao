2bf33d3eba30e9b7b1dd3d39a7bc218f7f8cf9ec
quan.cao <quan.cao@CAOQUAN1>
2018-10-25 19:28:47 +0800

    CreateHelloWorldProject

---
+++ HelloWorld-GitHub/HelloWorld-GitHub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input =Console.ReadLine();
+            Console.WriteLine(string.Format("HELLO WORLD -{0}", input));
+            Console.ReadKey();
+        }
+    }
+}
